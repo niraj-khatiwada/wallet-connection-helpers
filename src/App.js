c7a860c246f2c89ff58b9e87d63ff3a01daf6a79
niraj <nirazzz.nk@gmail.com>
2022-06-19 16:04:09 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import VanillaConnectionComponent from './VanillaConnectionComponent'
+import Web3ReactConnectionComponent from './Web3ReactConnectionComponent'
+
+window.onload = function () {
+  localStorage.removeItem('walletconnect')
+  localStorage.removeItem('WALLETCONNECT_DEEPLINK_CHOICE')
+}
+
+function App() {
+  const getLibrary = (provider) => {
+    const library = new Web3Provider(provider, 'any')
+    library.pollingInterval = 15000
+    return library
+  }
+
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="flex space-x-3">
+        <Web3ReactConnectionComponent />
+        <VanillaConnectionComponent />
+      </div>
+    </Web3ReactProvider>
+  )
+}
+
+export default App
